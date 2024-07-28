5d44a8374098ffda482d9642749e231a0e57d52c
Aditya Patil <addi1402@gmail.com>
2024-07-29 01:54:28 +0530

    refactor(header): improve header component ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,16 @@ import Tickbox from './Tickbox';
 
 export default function Header() {
   return (
-    <header className="border-b p-4 flex flex-col gap-3">
+    <header className="border-b p-6 pb-3 flex flex-col gap-6">
       <div>
-        <h3 className="font-semibold text-lg">User Details</h3>
-        <p className="text-sm text-neutral-500">Manage Your Customer Data</p>
+        <h3 className="font-semibold">Accounts</h3>
+        <p className="text-sm text-neutral-500">
+          Access Comprehensive Customer Details
+        </p>
       </div>
       <div className="flex justify-between">
         <SearchBar />
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <Dropdown />
           <Tickbox />
         </div>
--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,13 +1,12 @@
 import { useDispatch } from 'react-redux';
-import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { useEffect, useState } from 'react';
 import { AppDispatch } from '@/redux/store';
 import { nameFilter } from '@/redux/slices/userSlice';
 import useDebounce from '@/hooks/useDebounce';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 
 export default function SearchBar() {
-  // TODO: Insert an icon in search bar
   const [query, setQuery] = useState<string>('');
   const debouncedQuery = useDebounce(query, 1000);
   const dispatch = useDispatch<AppDispatch>();
@@ -16,21 +15,18 @@ export default function SearchBar() {
     dispatch(nameFilter(debouncedQuery));
   }, [debouncedQuery]);
 
-  function clearQuery() {
-    setQuery('');
-  }
-
   return (
     <div className="flex gap-2">
-      <Input
-        placeholder="Search User"
-        className="h-8"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <Button className="h-8" onClick={clearQuery}>
-        Clear
-      </Button>
+      <div className="relative ml-auto flex-1 md:grow-0">
+        <MagnifyingGlassIcon className="absolute left-2.5 top-2 h-5 text-neutral-400 w-5 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search..."
+          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
