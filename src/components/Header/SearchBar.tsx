21c6ddede4d5e915f38aea7465a37adc7ed4d606
Aditya Patil <addi1402@gmail.com>
2024-07-28 19:36:53 +0530

    feat(filter): add custom debounce hook and apply it to name filter

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -4,15 +4,17 @@ import { Input } from '../ui/input';
 import { useEffect, useState } from 'react';
 import { AppDispatch } from '@/redux/store';
 import { nameFilter } from '@/redux/slices/userSlice';
+import useDebounce from '@/hooks/useDebounce';
 
 export default function SearchBar() {
   // TODO: Insert an icon in search bar
   const [query, setQuery] = useState<string>('');
+  const debouncedQuery = useDebounce(query, 1000);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(nameFilter(query))
-  }, [query]);
+    dispatch(nameFilter(debouncedQuery));
+  }, [debouncedQuery]);
 
   function clearQuery() {
     setQuery('');
